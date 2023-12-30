032a931af7a08b02e88b622b7c92f557d3afcbb6
Nikolai Maksimov <kolkamaximov@gmail.com>
2023-12-30 21:14:40 +0300

    начальная версия проекта

---
+++ TrackList/TrackList/Controllers/TrackListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TrackListViewController.swift
+//  TrackList
+//
+//  Created by Nikolai Maksimov on 30.12.2023.
+//
+
+import UIKit
+
+final class TrackListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
