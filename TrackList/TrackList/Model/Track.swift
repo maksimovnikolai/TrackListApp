b30b5bc241f5659a59cbb71b3885cf8e8a75609a
Nikolai Maksimov <kolkamaximov@gmail.com>
2023-12-30 22:17:16 +0300

    добавлены исполниетели и плей-листы

--- TrackList/TrackList/Model/Track.swift
+++ TrackList/TrackList/Model/Track.swift
@@ -12,4 +12,19 @@ struct Track {
     var title: String {
         "\(artist) - \(song)"
     }
+    
+    static func getTrackList() -> [Track] {
+        [
+            Track(artist: "Alberto Ruiz", song: "7 Elements (Original Mix)"),
+            Track(artist: "Dave Wincent", song: "Red Eye (Original Mix)"),
+            Track(artist: "E-Spectro", song: "End Station (Original Mix)"),
+            Track(artist: "Edna Ann", song: "Phasma (Konstantin Yoodza Remix)"),
+            Track(artist: "Ilija Djokovic", song: "Delusion (Original Mix)"),
+            Track(artist: "John Baptiste", song: "Mycelium (Original Mix)"),
+            Track(artist: "Lane 8", song: "Fingerprint (Original Mix)"),
+            Track(artist: "Mac Vaughn", song: "Pink Is My Favorite Color (Alex Stein Remix)"),
+            Track(artist: "Metodi Hristov, Gallya", song: "Badmash (Original Mix)"),
+            Track(artist: "Veerus, Maxie Devine", song: "Nightmare (Original Mix)")
+        ]
+    }
 }
