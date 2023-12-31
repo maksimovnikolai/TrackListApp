9f1d74aa457b58391b3e23100890482e92af0d3c
Nikolai Maksimov <kolkamaximov@gmail.com>
2023-12-31 10:35:51 +0300

    добавлен tableView, методы делегата, констреинты

--- TrackList/TrackList/Controllers/TrackListViewController.swift
+++ TrackList/TrackList/Controllers/TrackListViewController.swift
@@ -9,8 +9,59 @@ import UIKit
 
 final class TrackListViewController: UIViewController {
     
+    
+    private lazy var tableView = makeTableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        commonInit()
     }
 }
+
+// MARK: - Private methods
+extension TrackListViewController {
+    
+    private func commonInit() {
+        title = "Track List"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        view.addSubview(tableView)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "track")
+        setupConstraints()
+    }
+    
+    private func makeTableView() -> UITableView {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.rowHeight = 80
+        return tableView
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension TrackListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "track", for: indexPath)
+        return cell
+    }
+}
+
+// MARK: - Constraints
+extension TrackListViewController {
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+}
+
+
