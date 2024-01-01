582c7c0be58f9d182744861673eb37532d75b3d7
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-01-01 14:06:21 +0300

    + uiimageview и констреинты

--- TrackList/TrackList/Controllers/TrackDetailsViewController.swift
+++ TrackList/TrackList/Controllers/TrackDetailsViewController.swift
@@ -9,8 +9,31 @@ import UIKit
 
 final class TrackDetailsViewController: UIViewController {
     
+    private lazy var coverImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = .white
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        setupConstraints()
     }
+}
+
+// MARK: - Constraints
+extension TrackDetailsViewController {
+    
+    private func setupConstraints() {
+        view.addSubview(coverImage)
+        NSLayoutConstraint.activate([
+            coverImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            coverImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            coverImage.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            coverImage.heightAnchor.constraint(equalToConstant: 330)
+        ])
+    }
+
 }
