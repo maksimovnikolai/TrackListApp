73e5112fb9a5cc3149241f8558eb9cc5ba7fcbed
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-01-01 13:52:34 +0300

    + track details controller

---
+++ TrackList/TrackList/Controllers/TrackDetailsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TrackDetailsViewController.swift
+//  TrackList
+//
+//  Created by Nikolai Maksimov on 01.01.2024.
+//
+
+import UIKit
+
+final class TrackDetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
